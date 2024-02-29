a627c70c0f468e6c6427d0055787e1d435b18fc8
Leo.Zhang <1053419428@qq.com>
2024-02-29 09:50:49 +0800

    Initialized gitee project.

--- app/src/main/java/com/leo/demo/rx/MainViewModel.kt
+++ app/src/main/java/com/leo/demo/rx/MainViewModel.kt
@@ -3,7 +3,6 @@ package com.leo.demo.rx
 import UNKNOWN_VOLUME
 import VolumeData
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
@@ -19,16 +18,9 @@ class MainViewModel {
     val listData = mutableStateOf(emptyList<VolumeData>())
 
     /**
-     *  Play publisher:
-     *  first: play mode
-     *  second: active volume
+     *  Publish the play mode [PLAY_MODE_MANUAL] or [PLAY_MODE_AUTO]. start with [STANDBY]
      */
     private val playModePublisher = PublishSubject.create<PlayMode>()
-
-    /**
-     *  Publisher of mock new volume received.
-     */
-    private val createVolumesPublisher = PublishSubject.create<List<VolumeData>>()
 
     /**
      *  The volumes has been played
@@ -46,17 +38,23 @@ class MainViewModel {
         Observable.combineLatest(
             listenToVolumes(), listenToVolumePlayMode(), listenToActiveVolume()
         ) { volumes, playMode, activeVolume ->
-            val list1 = updateReadState(volumes)
-            val list2 = updatePlayMode(playMode, activeVolume, list1)
-            val list3 = updatePlayState(playMode, activeVolume, list2)
-            list3
+            // The volumes there is no play info,
+            // so we need to update the volumes item play state and
+            // play mode with the second and third observable source.
+            //
+            // 1: update the volume with read state, if the volume is read, it will not play in auto mode.
+            // 2: update the volume with play mode, help to find the next play volume in auto mode.
+            // 3: update the volume with play state, to show in ui.
+            val readStateList = updateReadState(volumes)
+            val playModeList = updatePlayMode(playMode, activeVolume, readStateList)
+            val playStateList = updatePlayState(playMode, activeVolume, playModeList)
+            playStateList
         }.observeOn(AndroidSchedulers.mainThread()).subscribe({ volumes ->
             // Update UI
             listData.value = volumes
 
             // Play volume
-            play(volumes.find { volume -> volume.playState == READY })
-
+            volumes.find { READY == it.playState }?.let(::play)
         }, { e -> loge("Update ui failed, ${e.message}") })
     }
 
@@ -120,13 +118,18 @@ class MainViewModel {
         val list = volumes.toMutableList()
 
         when (playMode) {
-            // Play mode is standby, reset play state to standby.
+            // Standby mode:
+            //
+            // change all volumes play state to STANDBY.
             PLAY_MODE_STANDBY -> {
                 volumes.forEachIndexed { index, data ->
                     list[index] = data.copy(playState = STANDBY)
                 }
             }
 
+            // Manual mode:
+            //
+            // only update the active volume play state, others change to STANDBY.
             PLAY_MODE_MANUAL -> {
                 volumes.forEachIndexed { index, data ->
                     list[index] = if (data.timestamp == activeVolume.timestamp) {
@@ -137,8 +140,16 @@ class MainViewModel {
                 }
             }
 
+            // Auto mode:
+            //
+            // 1. when there is no active volume, make the first unread volume play state
+            // to READY, others change to STANDBY.
+            //
+            // 2. when the active volume play state is COMPLETE/FAILED find then next unread volume,
+            // if the next is found, make the next volume play state to READY, else all the volumes read.
+            // when the active volume play state is STANDBY/READY/PLAYING update the list.
             PLAY_MODE_AUTO -> {
-                // Play the first unread volume.
+                // 1. Make the first unread volume play state to READY.
                 if (activeVolume.timestamp == UNKNOWN_VOLUME) {
                     val first = volumes.find { it.playMode == PLAY_MODE_AUTO }
                     if (first != null) {
@@ -155,10 +166,10 @@ class MainViewModel {
                         logw("There is no unread volume.")
                     }
                 }
-                // find next unread volume
+                // 2. Find next unread volume.
                 else {
                     val next = volumes.find { it.playMode == PLAY_MODE_AUTO }
-                    // If the next is null, auto play complete.
+                    // If the next is null, auto play complete, make all volumes play state to STANDBY.
                     if (next == null) {
                         volumes.forEachIndexed { index, data ->
                             list[index] = data.copy(
@@ -170,7 +181,7 @@ class MainViewModel {
                             )
                         }
                     }
-                    // The active volume play complete, play the next
+                    // If the active volume play complete, make the next volume play state to STANDBY.
                     else if (activeVolume.playState in setOf(COMPLETED, FAILED)) {
                         volumes.forEachIndexed { index, data ->
                             list[index] = data.copy(
@@ -182,7 +193,7 @@ class MainViewModel {
                             )
                         }
                     }
-                    // Some things.
+                    // If the active volume in play state, update the lis.
                     else if (activeVolume.playState in setOf(READY, PLAYING, STANDBY)) {
                         volumes.forEachIndexed { index, data ->
                             list[index] = data.copy(
@@ -193,7 +204,9 @@ class MainViewModel {
                                 }
                             )
                         }
-                    } else {
+                    }
+                    // Set all volumes play state to STANDBY.
+                    else {
                         volumes.forEachIndexed { index, data ->
                             list[index] = data.copy(playState = STANDBY)
                         }
@@ -205,20 +218,16 @@ class MainViewModel {
         return list
     }
 
-    private fun listenToActiveVolume(): Observable<VolumeData> {
-        return activeVolumePublisher.startWith(Observable.just(VolumeData()))
-            .doOnNext { logw("Active volume play state update to ${it.playState}") }
+    private fun listenToVolumes(): Observable<List<VolumeData>> {
+        return Observable.just(createVolumes())
     }
 
-    private fun listenToVolumes(): Observable<List<VolumeData>> {
-        // volumes data creator.
-        return createInitVolumes().mergeWith(createVolumesPublisher)
-            .doOnNext { logw("Load new volumes, size is ${it.size}.") }
+    private fun listenToActiveVolume(): Observable<VolumeData> {
+        return activeVolumePublisher.startWith(Observable.just(VolumeData()))
     }
 
     private fun listenToVolumePlayMode(): Observable<PlayMode> {
         return playModePublisher.startWith(Observable.just(PLAY_MODE_STANDBY))
-            .doOnNext { logw("Volume play mode update to $it") }
     }
 
     fun manualPlay(data: VolumeData) {
@@ -235,39 +244,36 @@ class MainViewModel {
         activeVolumePublisher.onNext(VolumeData())
     }
 
-    fun addNewVolumes() {
-        createVolumesPublisher.onNext(createVolumes(5))
-    }
-
     private var playDisposable: Disposable? = null
 
     /**
-     *  Play the ready volume.
+     *  Only play the volume who's play state is [READY].
+     *
+     *  1: when subscribe publish [PLAYING] state to listener.
+     *  2: when play complete publish [COMPLETED] to listener.
+     *  3: when play failed, publish [FAILED] to listener.
      */
-    private fun play(data: VolumeData?) {
-        // If no active mode need to play, reset play mode to unknown
-        if (data == null) {
-            return
-        }
-
+    private fun play(data: VolumeData) {
         // Add to played list.
+        // In your project, this value should save to database to help make the read info persistent.
         readVolumes.add(data.timestamp)
 
         playDisposable?.dispose()
-        playDisposable = Single.just(data).delay(2, TimeUnit.SECONDS).doOnSubscribe {
-            activeVolumePublisher.onNext(data.copy(playState = PLAYING))
-        }.subscribe({
-            activeVolumePublisher.onNext(data.copy(playState = COMPLETED))
-        }, { e ->
-            activeVolumePublisher.onNext(data.copy(playState = FAILED))
-        })
+        playDisposable = Single.just(data)
+            // Mock play volume
+            .delay(2, TimeUnit.SECONDS)
+            // Publish PLAYING state to listener.
+            .doOnSubscribe {
+                activeVolumePublisher.onNext(data.copy(playState = PLAYING))
+            }.subscribe({
+                activeVolumePublisher.onNext(data.copy(playState = COMPLETED))
+            }, { e ->
+                loge("Play volume failed, ${e.message}")
+                activeVolumePublisher.onNext(data.copy(playState = FAILED))
+            })
     }
 
-    private fun createInitVolumes(): Observable<List<VolumeData>> {
-        return Observable.just(createVolumes(20))
-    }
-
-    private fun createVolumes(size: Int): List<VolumeData> {
+    private fun createVolumes(size: Int = 20): List<VolumeData> {
         val list = mutableListOf<VolumeData>()
         for (i in 1..size) {
             list.add(VolumeData(timestamp = System.currentTimeMillis() + i))
--- app/src/main/java/com/leo/demo/rx/PlayState.kt
+++ app/src/main/java/com/leo/demo/rx/PlayState.kt
@@ -2,5 +2,18 @@ package com.leo.demo.rx
 
 
 enum class PlayState {
-    READY, PLAYING, COMPLETED, FAILED, STANDBY
+    // The volume is ready to play.
+    READY,
+
+    // The volume is playing.
+    PLAYING,
+
+    // The volume play complete.
+    COMPLETED,
+
+    // The volume play failed.
+    FAILED,
+
+    // The volume is standby.
+    STANDBY
 }
