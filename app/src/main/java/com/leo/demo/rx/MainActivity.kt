a61b5cce3b8e0b4b8021d400ec2a7c6692f3684c
Leo.Zhang <1053419428@qq.com>
2024-02-29 09:55:41 +0800

    Initialized gitee project.

--- app/src/main/java/com/leo/demo/rx/MainActivity.kt
+++ app/src/main/java/com/leo/demo/rx/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -67,9 +70,18 @@ fun Greeting(viewModel: MainViewModel) {
                             viewModel.manualPlay(volume)
                         }) {
 
-                    Text(text = (if (volume.readState == ReadState.UNREAD) "(UNREAD)" else "") + volume.name)
+                    Text(text = volume.name)
 
-                    Text(text = volume.playState.name)
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = volume.playState.name)
+                        if (volume.readState == ReadState.UNREAD) {
+                            Spacer(modifier = Modifier.width(5.dp))
+                            AssistChip(label = { Text(text = "未读", color = Color.Red) },
+                                onClick = {})
+                        }
+                    }
                 }
             }
         }
